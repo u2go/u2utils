6dedbb6692507a01ef1bc56de906dcafbe013ec5
Echo Wang <wgqi1126@qq.com>
2022-03-30 17:16:51 +0800

    add RecoverError

--- error.go
+++ error.go
@@ -1,7 +1,30 @@
 package u2utils
 
+import (
+	"github.com/pkg/errors"
+	"go.uber.org/multierr"
+)
+
 func PanicOnError(err error) {
 	if err != nil {
 		panic(err)
+	}
+}
+
+func RecoverError(e interface{}, err error) error {
+	if e != nil {
+		var err1 error
+		if err0, ok := e.(error); ok {
+			err1 = err0
+		} else {
+			err1 = errors.Errorf("error: %+v", e)
+		}
+		if err != nil {
+			return multierr.Combine(err, err1)
+		} else {
+			return err
+		}
+	} else {
+		return err
 	}
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,13 @@ module github.com/u2go/u2utils
 
 go 1.18
 
-require github.com/satori/go.uuid v1.2.0
+require (
+	github.com/pkg/errors v0.9.1
+	github.com/satori/go.uuid v1.2.0
+	go.uber.org/multierr v1.8.0
+)
 
-require gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+require (
+	go.uber.org/atomic v1.7.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
