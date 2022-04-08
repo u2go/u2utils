831ed664d736200326a9b4496c3819cbecfc766e
Echo Wang <wgqi1126@qq.com>
2022-04-08 15:05:20 +0800

    add StrEncodeConvert

--- encode_test.go
+++ encode_test.go
@@ -3,9 +3,14 @@ package u2utils
 import (
 	"fmt"
 	"golang.org/x/text/encoding/simplifiedchinese"
+	"log"
 	"testing"
 )
 
 func TestStrEncodeConvert(t *testing.T) {
-	fmt.Println(string(StrEncodeConvert([]byte{180, 180, 210, 181, 176, 229}, simplifiedchinese.GB18030)))
+	r, err := StrEncodeConvert([]byte{180, 180, 210, 181, 176, 229}, simplifiedchinese.GB18030)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(string(r))
 }
