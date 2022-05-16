dd67a768ac78465ab0975ecea4dd3c147814cc34
Echo Wang <wgqi1126@qq.com>
2022-05-16 22:35:14 +0800

    cmder update

--- cmder/cmder.go
+++ cmder/cmder.go
@@ -11,26 +11,26 @@ import (
 type OutHandle func(outBuff []byte, allOutBytes []byte, inPipe io.WriteCloser) error
 
 // Run 执行命令，可以通过输出判断输入内容，实现命令交互
-func Run(name string, arg []string, outHandle, errHandle OutHandle) error {
+func Run(name string, arg []string, outHandle, errHandle OutHandle) (outBytes []byte, errBytes []byte, err error) {
 	cmd := exec.Command(name, arg...)
 	inPipe, err := cmd.StdinPipe()
 	if err != nil {
-		return err
+		return nil, nil, err
 	}
 	defer inPipe.Close()
 	outPipe, err := cmd.StdoutPipe()
 	if err != nil {
-		return err
+		return nil, nil, err
 	}
 	defer outPipe.Close()
 	errPipe, err := cmd.StderrPipe()
 	if err != nil {
-		return err
+		return nil, nil, err
 	}
 	defer errPipe.Close()
 	err = cmd.Start()
 	if err != nil {
-		return err
+		return nil, nil, err
 	}
 	wg := sync.WaitGroup{}
 	wg.Add(2)
@@ -38,23 +38,23 @@ func Run(name string, arg []string, outHandle, errHandle OutHandle) error {
 	var errErr error
 	go func() {
 		defer wg.Done()
-		outErr = handleOut(outPipe, inPipe, outHandle)
+		outBytes, outErr = handleOut(outPipe, inPipe, outHandle)
 	}()
 	go func() {
 		defer wg.Done()
-		errErr = handleOut(errPipe, inPipe, errHandle)
+		errBytes, errErr = handleOut(errPipe, inPipe, errHandle)
 	}()
 	wg.Wait()
 	if outErr != nil {
-		return outErr
+		return nil, nil, outErr
 	}
 	if errErr != nil {
-		return errErr
+		return nil, nil, errErr
 	}
-	return cmd.Wait()
+	return outBytes, errBytes, cmd.Wait()
 }
 
-func handleOut(out io.ReadCloser, in io.WriteCloser, handle OutHandle) error {
+func handleOut(out io.ReadCloser, in io.WriteCloser, handle OutHandle) ([]byte, error) {
 	var outBytes []byte
 	buf := make([]byte, 1024)
 	for {
@@ -63,16 +63,16 @@ func handleOut(out io.ReadCloser, in io.WriteCloser, handle OutHandle) error {
 			if errors.Is(err, io.EOF) {
 				break
 			} else {
-				return err
+				return outBytes, err
 			}
 		}
 		outBytes = append(outBytes, buf[:n]...)
 		if handle != nil {
 			err := handle(buf[:n], outBytes, in)
 			if err != nil {
-				return err
+				return outBytes, err
 			}
 		}
 	}
-	return nil
+	return outBytes, nil
 }
